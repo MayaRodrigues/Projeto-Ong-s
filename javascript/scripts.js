cc35b4e77803d5b007a80730999f955be3793db6
Mayara Rodrigues Pereira <pmayararodrigues@gmail.com>
2024-04-16 21:46:03 -0300

    commit 2

---
+++ javascript/scripts.js
@@ -0,0 +1,21 @@
+/*Animacao dos textos dos articles */
+const elementos=document.querySelectorAll('[data-anima]');
+
+const animacaoClass='animacao';
+
+function animaScroll(){
+    const topoPaginaNaJanela = window.pageYOffset +((window.innerHeight*3)/4);/*3/4 da janela */
+    elementos.forEach(function(elemento){
+        if(topoPaginaNaJanela > elemento.offsetTop){
+            elemento.classList.add(animacaoClass);
+        }else{
+            elemento.classList.remove(animacaoClass);
+        }
+    })
+}
+
+if(elementos.length){
+    window.addEventListener('scroll',function(){
+        animaScroll();
+    })
+}
